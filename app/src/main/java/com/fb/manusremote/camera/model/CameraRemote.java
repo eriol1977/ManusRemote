ce5c1dad1db16f4c793fdc95ec9f08fdb1767d78
francescobertolino <francesco.bertolino@gmail.com>
2015-01-29 14:55:11 +0100

    leitura do número de telefone

--- app/src/main/java/com/fb/manusremote/camera/model/CameraRemote.java
+++ app/src/main/java/com/fb/manusremote/camera/model/CameraRemote.java
@@ -9,8 +9,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
 import com.fb.manusremote.R;
 import com.fb.manusremote.camera.view.CameraRemoteActivity;
-import com.fb.manusremote.remote.AuthRequest;
 import com.fb.manusremote.remote.AbstractRemote;
+import com.fb.manusremote.remote.AuthRequest;
 
 import java.util.Map;
 
@@ -25,8 +25,11 @@ public class CameraRemote extends AbstractRemote {
     public final static String RECORD_VIDEO = "md.record.uploadftp";
     public final static String TAKE_PHOTO = "md.snapshot.enable";
     public final static String CALL_ENABLED = "md.sipphone.enable";
+    public final static String CALL_NUMBER = "phone.number";
 
     public final static String COMMAND_GET_MOTION_DETECTION_CONFIG = "goform/motiondetect?cmd=get";
+
+    public final static String COMMAND_GET_PHONEBOOK_CONFIG = "goform/sip?cmd=get";
 
     private final CameraRemoteActivity activity;
 
@@ -39,6 +42,10 @@ public class CameraRemote extends AbstractRemote {
     private boolean recordVideo;
 
     private boolean takePhoto;
+
+    private Map<String, String> motionDetectionConfiguration;
+
+    private Map<String, String> phonebookConfiguration;
 
     public CameraRemote(final Camera camera, final CameraRemoteActivity activity) {
         super(activity);
@@ -62,15 +69,13 @@ public class CameraRemote extends AbstractRemote {
                 new Response.Listener() {
                     @Override
                     public void onResponse(Object response) {
-                        final Map<String, String> motionDetectionConfiguration =
-                                buildConfigurationMap((String) response);
-                        motionDetection = getBooleanValue(motionDetectionConfiguration, MOTION_DETECTION);
-                        // TODO: ricavare il numero di telefono
+                        motionDetectionConfiguration = buildConfigurationMap((String) response);
                         final boolean callEnabled = getBooleanValue(motionDetectionConfiguration, CALL_ENABLED);
-                        callNumber = callEnabled ? "555" : "";
-                        recordVideo = getBooleanValue(motionDetectionConfiguration, RECORD_VIDEO);
-                        takePhoto = getBooleanValue(motionDetectionConfiguration, TAKE_PHOTO);
-                        activity.loadFields(CameraRemote.this);
+                        if (callEnabled) {
+                            loadPhonebookConfig();
+                        } else {
+                            fillProperties(false);
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -81,6 +86,43 @@ public class CameraRemote extends AbstractRemote {
         });
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
+    }
+
+    private void loadPhonebookConfig() {
+        final RequestQueue queue = Volley.newRequestQueue(activity);
+
+        final String url = buildURL(camera, COMMAND_GET_PHONEBOOK_CONFIG);
+
+        // Request a string response from the provided URL.
+        final AuthRequest stringRequest = new AuthRequest(camera.getUsername(), camera.getPassword(), Request.Method.GET, url,
+                new Response.Listener() {
+                    @Override
+                    public void onResponse(Object response) {
+                        phonebookConfiguration = buildConfigurationMap((String) response);
+                        fillProperties(true);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(activity, activity.getString(R.string.remote_config_loading_error), Toast.LENGTH_SHORT).show();
+                activity.showErrorMessage();
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    private void fillProperties(final boolean callEnabled) {
+        motionDetection = getBooleanValue(motionDetectionConfiguration, MOTION_DETECTION);
+        recordVideo = getBooleanValue(motionDetectionConfiguration, RECORD_VIDEO);
+        takePhoto = getBooleanValue(motionDetectionConfiguration, TAKE_PHOTO);
+
+        if (callEnabled)
+            callNumber = phonebookConfiguration.get(CALL_NUMBER);
+        else
+            callNumber = "";
+
+        activity.loadFields(CameraRemote.this);
     }
 
     @Override
